95cd112afe888c07abd4b92a5a6a4da061bf17a9
damianoo84 <damczechowski@gmail.com>
2022-07-24 22:06:10 +0200

    redirect changes

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -91,7 +91,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
     {
         if ($targetPath = $this->getTargetPath($request->getSession(), $providerKey)) {
-            return new RedirectResponse($targetPath);
+            return new RedirectResponse($this->urlGenerator->generate('liga_typerow_table'));
         }
 
         return new RedirectResponse($this->urlGenerator->generate('liga_typerow_table'));
