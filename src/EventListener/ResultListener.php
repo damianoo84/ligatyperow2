23853a9d0057c6f12d280d77b48cd2ad8efe00bd
Czechowski Damian - Partner Hurt <Damian.Czechowski@orange.com>
2022-11-24 23:42:25 +0100

    corrects in ResultListener

--- src/EventListener/ResultListener.php
+++ src/EventListener/ResultListener.php
@@ -42,8 +42,41 @@ class ResultListener {
                         )
                     {
                         $type->setNumberOfPoints(2);
-                } else {
-                        $type->setNumberOfPoints(0);
+                } elseif (
+                            // typ 2-1 wynik 0-0
+                            // typ 2-1 wynik 1-2
+                            // typ 1-1 wynik 2-1
+                            // typ 1-1 wynik 1-2
+                            // typ 1-2 wynik 1-1
+                            // typ 1-2 wynik 2-1
+                            
+                            // typ1 > typ2 i wynik1 == wynik2
+                            // typ1 > typ2 i wynik1 < wynik2
+                            // typ1 == typ2 i wynik1 > wynik2
+                            // typ1 == typ2 i wynik1 < wynik2
+                            // typ1 < typ2 i wynik1 == wynik2
+                            // typ1 < typ2 i wynik1 > wynik2
+                        
+                            (($type->getHostType() > $type->getGuestType()) &&
+                            ($entity->getHostGoals() == $entity->getGuestGoals())) || 
+                            
+                            (($type->getHostType() > $type->getGuestType()) &&
+                            ($entity->getHostGoals() < $entity->getGuestGoals())) || 
+                        
+                            (($type->getHostType() == $type->getGuestType()) &&
+                            ($entity->getHostGoals() > $entity->getGuestGoals())) || 
+                        
+                            (($type->getHostType() == $type->getGuestType()) &&
+                            ($entity->getHostGoals() < $entity->getGuestGoals())) || 
+                        
+                            (($type->getHostType() < $type->getGuestType()) &&
+                            ($entity->getHostGoals() == $entity->getGuestGoals())) || 
+                        
+                            (($type->getHostType() < $type->getGuestType()) &&
+                            ($entity->getHostGoals() > $entity->getGuestGoals()))
+                        
+                ) {
+                    $type->setNumberOfPoints(0);
                 }
                 
                 $em->persist($type);
