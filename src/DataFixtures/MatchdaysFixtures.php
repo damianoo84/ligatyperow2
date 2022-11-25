6013590b79042c285c50ea17306fd9798c40485a
Czechowski Damian - Partner Hurt <Damian.Czechowski@orange.com>
2022-11-25 23:53:13 +0100

    changes in ResultListener

--- src/DataFixtures/MatchdaysFixtures.php
+++ src/DataFixtures/MatchdaysFixtures.php
@@ -18,92 +18,92 @@ class MatchdaysFixtures extends Fixture implements OrderedFixtureInterface{
         $matchdaysList = array(
             array(
                 'matchday_name' => '1',
-                'dateFrom' => '2022-08-01',
-                'dateTo' => '2022-08-07',
+                'dateFrom' => '2022-11-27',
+                'dateTo' => '2022-11-29',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '2',
-                'dateFrom' => '2022-08-08',
-                'dateTo' => '2022-08-14',
+                'dateFrom' => '2022-11-30',
+                'dateTo' => '2022-12-01',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '3',
-                'dateFrom' => '2022-08-15',
-                'dateTo' => '2022-08-21',
+                'dateFrom' => '2022-12-15',
+                'dateTo' => '2022-12-21',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '4',
-                'dateFrom' => '2022-08-22',
-                'dateTo' => '2022-08-28',
+                'dateFrom' => '2022-12-22',
+                'dateTo' => '2022-12-28',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '5',
-                'dateFrom' => '2022-08-29',
-                'dateTo' => '2022-09-04',
+                'dateFrom' => '2023-08-29',
+                'dateTo' => '2023-09-04',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '6',
-                'dateFrom' => '2022-09-05',
-                'dateTo' => '2022-09-11',
+                'dateFrom' => '2023-09-05',
+                'dateTo' => '2023-09-11',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '7',
-                'dateFrom' => '2022-09-12',
-                'dateTo' => '2022-09-18',
+                'dateFrom' => '2023-09-12',
+                'dateTo' => '2023-09-18',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '8',
-                'dateFrom' => '2022-09-19',
-                'dateTo' => '2022-09-25',
+                'dateFrom' => '2023-09-19',
+                'dateTo' => '2023-09-25',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '9',
-                'dateFrom' => '2022-09-26',
-                'dateTo' => '2022-10-02',
+                'dateFrom' => '2023-09-26',
+                'dateTo' => '2023-10-02',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '10',
-                'dateFrom' => '2022-10-03',
-                'dateTo' => '2022-10-09',
+                'dateFrom' => '2023-10-03',
+                'dateTo' => '2023-10-09',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '11',
-                'dateFrom' => '2022-10-10',
-                'dateTo' => '2022-10-16',
+                'dateFrom' => '2023-10-10',
+                'dateTo' => '2023-10-16',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '12',
-                'dateFrom' => '2022-10-17',
-                'dateTo' => '2022-10-23',
+                'dateFrom' => '2023-10-17',
+                'dateTo' => '2023-10-23',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '13',
-                'dateFrom' => '2022-10-24',
-                'dateTo' => '2022-10-30',
+                'dateFrom' => '2023-10-24',
+                'dateTo' => '2023-10-30',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '14',
-                'dateFrom' => '2022-10-31',
-                'dateTo' => '2022-11-06',
+                'dateFrom' => '2023-10-31',
+                'dateTo' => '2023-11-06',
                 'season_name' => 'Jesień 2022'
             ),
             array(
                 'matchday_name' => '15',
-                'dateFrom' => '2022-11-07',
-                'dateTo' => '2022-11-13',
+                'dateFrom' => '2023-11-07',
+                'dateTo' => '2023-11-13',
                 'season_name' => 'Jesień 2022'
             )
         );
--- src/EventListener/ResultListener.php
+++ src/EventListener/ResultListener.php
@@ -5,83 +5,135 @@ namespace App\EventListener;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use App\Entity\Meet;
 use App\Entity\Type;
+use Psr\Log\LoggerInterface;
 
 class ResultListener {
+    
+    private $logger;
+    
+    public function __construct(LoggerInterface $logger)
+    {
+        $this->logger = $logger;
+    }
     
     public function postUpdate(LifecycleEventArgs $args) {
         $entity = $args->getEntity();
         $em = $args->getEntityManager();
         
         if ($entity instanceof Meet) {
+            
             $meetId = $entity->getId();
             $typeRepo = $em->getRepository(Type::class);
             $types = $typeRepo->findByMeet($meetId);
             
             foreach ($types as $type) {
-                if (($type->getHostType() == $entity->getHostGoals()) && ($type->getGuestType() == $entity->getGuestGoals())){
-                        $type->setNumberOfPoints(4);
-                } elseif (
-                            // typ1 > typ2 i wynik1 > wynik2
-                            ($type->getHostType() > $type->getGuestType()) && 
-                            ($entity->getHostGoals() > $entity->getGuestGoals()) 
-                        )
-                    {
-                        $type->setNumberOfPoints(2);
-                } elseif (
-                            // typ1 < typ2 i wynik1 < wynik2
-                            ($type->getHostType() < $type->getGuestType()) && 
-                            ($entity->getHostGoals() < $entity->getGuestGoals())
-                        )
-                    {
-                        $type->setNumberOfPoints(2);
-                } elseif (
-                            // typ1 == typ2 i wynik1 == wynik2 i typ1 <> wynik1
-                            ($type->getHostType() == $type->getGuestType()) && 
-                            ($entity->getHostGoals() == $entity->getGuestGoals()) && 
-                            ($type->getHostType() <> $entity->getHostGoals())
-                        )
-                    {
-                        $type->setNumberOfPoints(2);
-                } elseif (
-                            // typ 2-1 wynik 0-0
-                            // typ 2-1 wynik 1-2
-                            // typ 1-1 wynik 2-1
-                            // typ 1-1 wynik 1-2
-                            // typ 1-2 wynik 1-1
-                            // typ 1-2 wynik 2-1
-                            
-                            // typ1 > typ2 i wynik1 == wynik2
-                            // typ1 > typ2 i wynik1 < wynik2
-                            // typ1 == typ2 i wynik1 > wynik2
-                            // typ1 == typ2 i wynik1 < wynik2
-                            // typ1 < typ2 i wynik1 == wynik2
-                            // typ1 < typ2 i wynik1 > wynik2
-                        
-                            (($type->getHostType() > $type->getGuestType()) &&
-                            ($entity->getHostGoals() == $entity->getGuestGoals())) || 
-                            
-                            (($type->getHostType() > $type->getGuestType()) &&
-                            ($entity->getHostGoals() < $entity->getGuestGoals())) || 
-                        
-                            (($type->getHostType() == $type->getGuestType()) &&
-                            ($entity->getHostGoals() > $entity->getGuestGoals())) || 
-                        
-                            (($type->getHostType() == $type->getGuestType()) &&
-                            ($entity->getHostGoals() < $entity->getGuestGoals())) || 
-                        
-                            (($type->getHostType() < $type->getGuestType()) &&
-                            ($entity->getHostGoals() == $entity->getGuestGoals())) || 
-                        
-                            (($type->getHostType() < $type->getGuestType()) &&
-                            ($entity->getHostGoals() > $entity->getGuestGoals()))
-                        
-                ) {
-                    $type->setNumberOfPoints(0);
-                }
                 
-                $em->persist($type);
+                    if (
+                            ($type->getHostType() == $entity->getHostGoals()) && 
+                            ($type->getGuestType() == $entity->getGuestGoals())
+                        )
+                    {
+                        $this->logger->info('Update set 4 points');
+                        $type->setNumberOfPoints(4);
+                    } elseif (
+                                // typ1 > typ2 i wynik1 > wynik2
+                                ($type->getHostType() > $type->getGuestType()) && 
+                                ($entity->getHostGoals() > $entity->getGuestGoals())
+                             )
+                    {
+                        $this->logger->info('Update set 2 points (option 1)');
+                        $type->setNumberOfPoints(2);
+                    } elseif (
+                                // typ1 < typ2 i wynik1 < wynik2
+                                ($type->getHostType() < $type->getGuestType()) && 
+                                ($entity->getHostGoals() < $entity->getGuestGoals())
+                             )
+                    {
+                        $this->logger->info('Update set 2 points (option 2)');
+                        $type->setNumberOfPoints(2);
+                    } elseif (
+                                // typ1 == typ2 i wynik1 == wynik2 i typ1 <> wynik1
+                                ($type->getHostType() == $type->getGuestType()) && 
+                                ($entity->getHostGoals() == $entity->getGuestGoals()) && 
+                                ($type->getHostType() <> $entity->getHostGoals())
+                             )
+                    {
+                        $this->logger->info('$type->getHostType(): ' . $type->getHostType());
+                        $this->logger->info('$type->getGuestType(): ' . $type->getGuestType());
+                        $this->logger->info('$entity->getHostGoals(): ' . $entity->getHostGoals());
+                        $this->logger->info('$entity->getGuestGoals(): ' . $entity->getGuestGoals());
+                        $this->logger->info('Update set 2 points (option 3)');
+                        
+                        if ($entity->getHostGoals() == '') {
+                            $this->logger->info('test1'); // tak
+                        }
+                        
+                        if ($entity->getHostGoals() == NULL) {
+                            $this->logger->info('test2'); // tak
+                        }
+                        
+                        if ($entity->getHostGoals() == "") {
+                            $this->logger->info('test3'); // tak
+                        }
+                        
+                        if ($entity->getHostGoals() == ' ') {
+                            $this->logger->info('test4');
+                        }
+                        
+                        if ($entity->getHostGoals() == " ") {
+                            $this->logger->info('test5');
+                        } 
+                        
+                        if (empty($entity->getHostGoals())) {
+                            $this->logger->info('test6'); // tak
+                        }
+                        
+                        $type->setNumberOfPoints(2);     
+                    } elseif (
+                                // typ 2-1 wynik 0-0
+                                // typ 2-1 wynik 1-2
+                                // typ 1-1 wynik 2-1
+                                // typ 1-1 wynik 1-2
+                                // typ 1-2 wynik 1-1
+                                // typ 1-2 wynik 2-1
+
+                                // typ1 > typ2 i wynik1 == wynik2
+                                // typ1 > typ2 i wynik1 < wynik2
+                                // typ1 == typ2 i wynik1 > wynik2
+                                // typ1 == typ2 i wynik1 < wynik2
+                                // typ1 < typ2 i wynik1 == wynik2
+                                // typ1 < typ2 i wynik1 > wynik2
+
+                                (($type->getHostType() > $type->getGuestType()) &&
+                                ($entity->getHostGoals() == $entity->getGuestGoals())) || 
+
+                                (($type->getHostType() > $type->getGuestType()) &&
+                                ($entity->getHostGoals() < $entity->getGuestGoals())) || 
+
+                                (($type->getHostType() == $type->getGuestType()) &&
+                                ($entity->getHostGoals() > $entity->getGuestGoals())) || 
+
+                                (($type->getHostType() == $type->getGuestType()) &&
+                                ($entity->getHostGoals() < $entity->getGuestGoals())) || 
+
+                                (($type->getHostType() < $type->getGuestType()) &&
+                                ($entity->getHostGoals() == $entity->getGuestGoals())) || 
+
+                                (($type->getHostType() < $type->getGuestType()) &&
+                                ($entity->getHostGoals() > $entity->getGuestGoals())) 
+                            ) 
+                    {
+                        $this->logger->info('Update set 0 points');
+                        $type->setNumberOfPoints(0);
+                    } else {
+                        $this->logger->info('NO UPDATE!');
+                    }
+
+                    $em->persist($type);
+                    $em->flush();
             }
-            $em->flush();
+            
+//            $em->flush();
         }
     } 
 }
